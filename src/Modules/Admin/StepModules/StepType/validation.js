f064a1ffe9e2aabdfdbc7ad5b83cf72ff5349ff2
bespokecashmeres <bespokecashmeres@gmail.com>
2024-12-28 13:32:46 +0530

    Removed showFittingOption, as not needed any more

--- src/Modules/Admin/StepModules/StepType/validation.js
+++ src/Modules/Admin/StepModules/StepType/validation.js
@@ -6,7 +6,6 @@ exports.createValidator = Joi.object({
   name: Joi.string().required(),
   info: Joi.string().required(),
   productTypeId: Joi.string().required(),
-  showFittingOption: Joi.boolean().optional().default(false),
   status: statusJoiValidation,
 });
 
@@ -14,7 +13,6 @@ exports.updateValidator = Joi.object({
   _id: Joi.string().hex().length(24).required(),
   name: Joi.string().required(),
   info: Joi.string().required(),
-  showFittingOption: Joi.boolean().optional().default(false),
 });
 
 exports.tabsValidator = Joi.object({
